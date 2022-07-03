0ce1c56d07fc266205d8bd81deaa3f44c8d323cc
Pjota94 <juniorgurinhem9@gmail.com>
2022-07-03 20:30:25 -0300

    Feat: Finish

--- src/controller/Index.controller.js
+++ src/controller/Index.controller.js
@@ -4,7 +4,7 @@ import Foto from "../models/Foto.model.js";
 class Index{
     static async listarFotos(){
         const ul = document.querySelector('#cards');
-        // ul.innerHTML = "";
+        ul.innerHTML = "";
         const arrayDados = await Requisicoes.getDados()
         
         arrayDados.forEach((card) => {
@@ -16,6 +16,8 @@ class Index{
     static formulario(){
         const boxForm = document.querySelector('.box-form')
         const form = document.createElement('form');
+        form.id = 'form';
+
         form.addEventListener('submit', async (event)=>{
             event.preventDefault()
             const data = {};
@@ -26,33 +28,28 @@ class Index{
             })
             await Requisicoes.adicionarFoto(data)
             this.listarFotos()
-    
-    
         })
-
-
         //Criando Elementos
         const h3 = document.createElement('h3');
+        h3.innerText = 'Adicionar uma Nova Imagem';
+
         const inputTitulo = document.createElement('input');
-        const inputImagem = document.createElement('input');
-        const inputDescricao = document.createElement('textarea');
-        const button = document.createElement('button')
-        //Adicionando Placeholder
         inputTitulo.placeholder = 'Titulo';
+        inputTitulo.name = 'titulo';
+
+        const inputImagem = document.createElement('input');
         inputImagem.placeholder = 'Url de Imagem';
+        inputImagem.name = 'imagem';
+
+        const inputDescricao = document.createElement('textarea');
         inputDescricao.placeholder = 'Escreva uma descricao';
         inputDescricao.cols = '30';
         inputDescricao.rows = '5';
-        //Adicionando texto
-        h3.innerText = 'Adicionar uma Nova Imagem';
-        button.innerText = 'Adicionar Imagem';
-        //Adicionando Ids e Classes
-        form.id = 'form';
-        button.classList.add('btn');
-        //Adicionando Name
-        inputTitulo.name = 'titulo';
-        inputImagem.name = 'imagem';
         inputDescricao.name = 'descricao';
+
+        const button = document.createElement('button')
+        button.innerText = 'Adicionar Imagem';
+        button.classList.add('btn');
         button.type = 'submit';
         //Inserindo no HTML
         form.append(h3,inputTitulo,inputImagem,inputDescricao,button);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,5 @@ import Requisicoes from "../controller/Requisicoes.controller.js";
 import Index from "../controller/Index.controller.js"
 
 
-await Index.formulario()
+Index.formulario()
 Index.listarFotos()
